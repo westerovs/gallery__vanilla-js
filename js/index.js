a5ed2f2167975e82f4ff9b0a380476737fbfe28f
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2021-02-01 17:36:06 +0300

    сделал переключÐение на боковых кнопках

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
-class Galery {
+class Gallery {
     constructor() {
-        this.buttons = document.querySelectorAll('.gallery__card-item')
-        this.tabs = document.querySelectorAll('.gallery__main-content')
+        // верхние контроллы
+        this.galleryCoverBtns = document.querySelectorAll('.gallery__cover-item')
+        this.galleryCoverMain = document.querySelectorAll('.gallery__cover-main')
+
+        // нижние контроллы
+        this.galleryCardBtns = document.querySelectorAll('.gallery__card-item')
+        this.galleryMainContents = document.querySelectorAll('.gallery__main-content')
+
+        this.src = null
+    }
+
+    getImageSrc = () => {
+        this.galleryCoverBtns.forEach(item => {
+            item.addEventListener('pointerdown', () => {
+                this.src = item.querySelector('img').getAttribute('src')
+                this.changeCoverImage()
+            })
+        })
+    }
+
+    changeCoverImage = () => {
+        console.log(this.src)
+
+        this.galleryCoverMain.forEach(item => {
+            if (item.classList.contains('is-active')) {
+                item.setAttribute('src', this.src)
+            }
+        })
     }
 
     clearActiveClass = (element, nameClass = 'is-active') => {
@@ -12,23 +38,33 @@ class Galery {
         element[index].classList.add(`${ nameClass }`)
     }
 
-    elementClick = (element = this.buttons, nameClass = 'is-active') => {
+    elementClick = (element = this.galleryCardBtns, nameClass = 'is-active') => {
         element.forEach((item, index) => {
             item.addEventListener('pointerdown', () => {
 
                 if (item.classList.contains(nameClass)) return
 
-                this.clearActiveClass(this.buttons)
-                this.clearActiveClass(this.tabs)
-                this.setActiveClass(this.buttons, index)
-                this.setActiveClass(this.tabs, index)
+                this.clearActiveClass(this.galleryCardBtns)
+                this.clearActiveClass(this.galleryMainContents)
+                this.clearActiveClass(this.galleryCoverMain)
+
+                this.setActiveClass(this.galleryCardBtns, index)
+                this.setActiveClass(this.galleryMainContents, index)
+                this.setActiveClass(this.galleryCoverMain, index)
             })
         })
     }
+
+    init = () => {
+        console.log(' gallery init')
+        this.getImageSrc()
+        this.elementClick()
+        this.getImageSrc()
+    }
 }
 
-const galery = new Galery()
-galery.elementClick()
+const gallery = new Gallery()
+gallery.init()
 
 
 
