abb8a69e3f96874280c9f97d371ee1972d8945ab
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2021-02-01 15:58:02 +0300

    ===== захардкодил клик по контенте =====

--- js/index.js
+++ js/index.js
@@ -1,4 +1,35 @@
-/* eslint-disable */
+class Galery {
+    constructor() {
+        this.buttons = document.querySelectorAll('.gallery__card-item')
+        this.tabs = document.querySelectorAll('.gallery__main-content')
+    }
+
+    clearActiveClass = (element, nameClass = 'is-active') => {
+        element.forEach(item => item.classList.remove(`${ nameClass }`))
+    }
+
+    setActiveClass = (element, index, nameClass = 'is-active') => {
+        element[index].classList.add(`${ nameClass }`)
+    }
+
+    elementClick = (element = this.buttons, nameClass = 'is-active') => {
+        element.forEach((item, index) => {
+            item.addEventListener('pointerdown', () => {
+
+                if (item.classList.contains(nameClass)) return
+
+                this.clearActiveClass(this.buttons)
+                this.clearActiveClass(this.tabs)
+                this.setActiveClass(this.buttons, index)
+                this.setActiveClass(this.tabs, index)
+            })
+        })
+    }
+}
+
+const galery = new Galery()
+galery.elementClick()
+
 
 
 
